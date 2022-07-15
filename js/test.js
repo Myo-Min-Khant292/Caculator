5fa6cd9c6c6c6ac4d64e9664a05326ffec42fad5
SixMJ <103758712+Mr-Khant@users.noreply.github.com>
2022-07-15 16:21:47 +0630

    Fully Functional Caculator

--- js/test.js
+++ js/test.js
@@ -1,1 +1,22 @@
 let result = document.getElementById('result') ;
+
+function display(num) {
+	result.value += num
+}
+
+function caculate() {
+	try {
+		result.value = eval(result.value)
+	}
+	catch (err) {
+		alert(err)
+	}
+}
+
+function Clear() {
+	result.value = " ";
+}
+
+function del() {
+	result.value = result.value.slice(0,-1)
+}
